514a45bcbae735afda36558110fefc96dbde3300
Nishit R. Acharya <40692435+nishitr3103@users.noreply.github.com>
2022-01-19 19:57:07 +0530

    Update mongoose.js

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-const url = 'mongodb+srv://ecommerce_api:nodejs@cluster1.pkihm.mongodb.net/Ecommerce_api?retryWrites=true&w=majority';
+const url = '';
 
 mongoose.connect(url,{
     useNewUrlParser: true
